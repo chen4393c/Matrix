3fa4b532e01367db3ee636f31b93f5fdd929b946
chen4393c <chen4393c@gmail.com>
2018-09-19 21:40:17 -0700

    class 4

--- app/src/main/java/com/laioffer/matrix/LoginFragment.java
+++ app/src/main/java/com/laioffer/matrix/LoginFragment.java
@@ -5,6 +5,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -13,6 +22,14 @@ public class LoginFragment extends Fragment {
 
     private View loginLayout;
     private View logoutLayout;
+    private EditText mUsernameEditText;
+    private EditText mPasswordEditText;
+    private Button mSubmitButton;
+    private Button mRegisterButton;
+    private Button mLogoutButton;
+
+    private DatabaseReference mDatabase;
+
 
     /**
      * Static function, create loginFragment instance
@@ -36,6 +53,88 @@ public class LoginFragment extends Fragment {
         loginLayout = view.findViewById(R.id.loginLayout);
         logoutLayout = view.findViewById(R.id.logoutLayout);
         showLayout();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mUsernameEditText = (EditText) view.findViewById(R.id.editTextLogin);
+        mPasswordEditText = (EditText) view.findViewById(R.id.editTextPassword);
+        mSubmitButton = (Button) view.findViewById(R.id.submit);
+        mRegisterButton = (Button) view.findViewById(R.id.register);
+        mLogoutButton = (Button) view.findViewById(R.id.logout);
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = mUsernameEditText.getText().toString();
+                final String password = mPasswordEditText.getText().toString();
+
+                mDatabase.child("user").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.hasChild(username)) {
+                            Toast.makeText(getActivity(),
+                                    "username is already registered, please change one",
+                                    Toast.LENGTH_SHORT).show();
+                        } else if (!username.equals("") && !password.equals("")){
+                            // put username as key to set value
+                            final User user = new User();
+                            user.setUser_account(username);
+                            user.setUser_password(Utils.md5Encryption(password));
+                            user.setUser_timestamp(System.currentTimeMillis());
+
+                            mDatabase.child("user").child(user.getUser_account()).setValue(user);
+                            Toast.makeText(getActivity(),
+                                    "Successfully registered",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = mUsernameEditText.getText().toString();
+                final String password = Utils.md5Encryption(mPasswordEditText.getText().toString());
+
+                mDatabase.child("user").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.hasChild(username) &&
+                                (password.equals(dataSnapshot
+                                        .child(username)
+                                        .child("user_password")
+                                        .getValue()))) {
+                            Config.username = username;
+                            showLayout();
+                        } else {
+                            Toast.makeText(getActivity(),
+                                    "Please login again",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+        mLogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Config.username = null;
+                showLayout();
+            }
+        });
+
         return view;
     }
 
